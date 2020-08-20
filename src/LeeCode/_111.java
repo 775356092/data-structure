6a1a9d66739465ad899e4302e8939f2c36b47360
Jerry <775356092@qq.com>
2020-08-21 00:19:04 +0800

    二叉树的最小深度

---
+++ src/LeeCode/_111.java
@@ -0,0 +1,28 @@
+package LeeCode;
+
+public class _111 {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        // null节点不参与比较
+        if (root.left == null && root.right != null) {
+            return 1 + minDepth(root.right);
+        }
+        // null节点不参与比较
+        if (root.right == null && root.left != null) {
+            return 1 + minDepth(root.left);
+        }
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
