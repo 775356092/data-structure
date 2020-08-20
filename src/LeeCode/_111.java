6d27a4d1b74ca566b3d3dfd530ceec4dd0ebc813
Jerry <775356092@qq.com>
2020-08-21 00:53:59 +0800

    二叉树的最小深度

--- src/LeeCode/_111.java
+++ src/LeeCode/_111.java
@@ -1,6 +1,10 @@
 package LeeCode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class _111 {
+    //dfs
     public int minDepth(TreeNode root) {
         if (root == null) {
             return 0;
@@ -14,6 +18,33 @@ public class _111 {
             return 1 + minDepth(root.left);
         }
         return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+
+    //bfs
+    public int mDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int res = 0;
+        while (!queue.isEmpty()) {
+            res++;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left == null && node.right == null) {
+                    return res;
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+        return 0;
     }
 
     public class TreeNode {
