79fdb9989973e9d97321f181ed0a113077e34d9b
wanghao <wh89801227@163.com>
2020-08-21 16:15:28 +0800

    first commit

--- src/LeetCode/_104.java
+++ src/LeetCode/_104.java
@@ -1,6 +1,7 @@
 package LeetCode;
 
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class _104 {
     /**
@@ -8,38 +9,37 @@ public class _104 {
      * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
      */
 
-    //DFS
-    public int maxDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+//DFS
+public int maxDepth(TreeNode root) {
+    if (root == null) {
+        return 0;
     }
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+}
 
-    //BFS
-    public int depth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        LinkedList<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        LinkedList<TreeNode> temp;
-        int res = 0;
-        while (!queue.isEmpty()) {
-            temp = new LinkedList<>();
-            for (TreeNode node : queue) {
-                if (node.left != null) {
-                    temp.add(node.left);
-                }
-                if (node.right != null) {
-                    temp.add(node.right);
-                }
-            }
-            queue = temp;
-            res++;
-        }
-        return res;
+//BFS
+public int depth(TreeNode root) {
+    if (root == null) {
+        return 0;
     }
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.add(root);
+    int res = 0;
+    while (!queue.isEmpty()) {
+        res++;
+        int size = queue.size();
+        for (int i = 0; i < size; i++) {
+            TreeNode node = queue.poll();
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+    }
+    return res;
+}
 
     public class TreeNode {
         int val;
--- src/LeetCode/_111.java
+++ src/LeetCode/_111.java
@@ -5,47 +5,45 @@ import java.util.Queue;
 
 public class _111 {
     //dfs
-    public int minDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        // null节点不参与比较
-        if (root.left == null && root.right != null) {
-            return 1 + minDepth(root.right);
-        }
-        // null节点不参与比较
-        if (root.right == null && root.left != null) {
-            return 1 + minDepth(root.left);
-        }
-        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
-    }
-
-    //bfs
-    public int mDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        int res = 0;
-        while (!queue.isEmpty()) {
-            res++;
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                if (node.left == null && node.right == null) {
-                    return res;
-                }
-                if (node.left != null) {
-                    queue.offer(node.left);
-                }
-                if (node.right != null) {
-                    queue.offer(node.right);
-                }
-            }
-        }
+public int minDepth(TreeNode root) {
+    if (root == null) {
         return 0;
     }
+    if (root.left == null && root.right != null) {
+        return 1 + minDepth(root.right);
+    }
+    if (root.right == null && root.left != null) {
+        return 1 + minDepth(root.left);
+    }
+    return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+}
+
+    //bfs
+public int mDepth(TreeNode root) {
+    if (root == null) {
+        return 0;
+    }
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+    int res = 0;
+    while (!queue.isEmpty()) {
+        res++;
+        int size = queue.size();
+        for (int i = 0; i < size; i++) {
+            TreeNode node = queue.poll();
+            if (node.left == null && node.right == null) {
+                return res;
+            }
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+    }
+    return 0;
+}
 
     public class TreeNode {
         int val;
