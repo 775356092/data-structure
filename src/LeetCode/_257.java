07e325f5bfda185066459063ac906524b6573d70
wanghao <wh89801227@163.com>
2020-09-04 10:10:36 +0800

    二叉树的所有路径

--- src/LeetCode/_257.java
+++ src/LeetCode/_257.java
@@ -1,5 +1,8 @@
 package LeetCode;
 
+import org.junit.Test;
+
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,8 +23,34 @@ public class _257 {
     }
 
     ArrayList<String> res = new ArrayList<>();
+    ArrayList<Integer> path = new ArrayList<>();
 
     public List<String> binaryTreePaths(TreeNode root) {
+        dfs(root);
+        return res;
+    }
+
+    public void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        path.add(root.val);
+        if (root.left == null && root.right == null) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < path.size(); i++) {
+                sb.append(path.get(i));
+                if (i != path.size() - 1) {
+                    sb.append("->");
+                }
+            }
+            res.add(sb.toString());
+        }
+        dfs(root.left);
+        dfs(root.right);
+        path.remove(path.size() - 1);
+    }
+
+    /*public List<String> binaryTreePaths(TreeNode root) {
         dfs(root, "");
         return res;
     }
@@ -30,14 +59,23 @@ public class _257 {
         if (root == null) {
             return;
         }
-        s += (root.val + "");
+        s += root.val + "";
         if (root.left == null && root.right == null) {
             res.add(s);
-            return;
-        } else {
-            s += "->";
-            dfs(root.left, s);
-            dfs(root.right, s);
         }
+        s += "->";
+        dfs(root.left, s);
+        dfs(root.right, s);
+    }*/
+
+    @Test
+    public void Test() {
+        TreeNode root = new TreeNode(1);
+        TreeNode node1 = new TreeNode(2);
+        TreeNode node2 = new TreeNode(3);
+        root.left = node1;
+        root.right = node2;
+        List<String> list = binaryTreePaths(root);
+        System.out.println(list);
     }
 }
