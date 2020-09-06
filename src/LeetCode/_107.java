beab14fff2563825fbebb716edb3c119bb986b05
Jerry <775356092@qq.com>
2020-09-06 13:08:46 +0800

    二叉树的层序遍历（自底向上）

---
+++ src/LeetCode/_107.java
@@ -0,0 +1,63 @@
+package LeetCode;
+
+import org.junit.Test;
+
+import java.util.*;
+
+/**
+ * 二叉树的层序遍历（自底向上）
+ */
+
+public class _107 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        ArrayList<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            ArrayList<Integer> temp = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                temp.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            res.add(temp);
+        }
+        Collections.reverse(res);
+        return res;
+    }
+
+    @Test
+    public void test(){
+        TreeNode root = new TreeNode(3);
+        TreeNode node1 = new TreeNode(9);
+        TreeNode node2 = new TreeNode(20);
+        TreeNode node3 = new TreeNode(15);
+        TreeNode node4 = new TreeNode(7);
+        root.left = node1;
+        root.right = node2;
+        node2.left = node3;
+        node2.right = node4;
+        List<List<Integer>> lists = levelOrderBottom(root);
+        System.out.println(lists);
+    }
+}
