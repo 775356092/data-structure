fdaa2ad382a239194875435c4656fe5275348276
wanghao <wh89801227@163.com>
2020-09-08 16:25:28 +0800

    实现strStr()

--- src/LeetCode/_28.java
+++ src/LeetCode/_28.java
@@ -8,14 +8,25 @@ package LeetCode;
 public class _28 {
     /**
      * 实现 strStr() 函数。
-     *
-     * 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
-     *
+     * <p>
+     * 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回-1。
      */
     public int strStr(String haystack, String needle) {
-        if(needle==null||needle.equals("")){
+        if (needle == null || needle.equals("")) {
             return 0;
         }
-        return 0;
+        char[] s = haystack.toCharArray();
+        char[] p = needle.toCharArray();
+        for (int i = 0; i <= s.length - p.length; i++) {
+            for (int j = 0; j < p.length; j++) {
+                if (s[i + j] != p[j]) {
+                    break;
+                }
+                if (j == p.length - 1) {
+                    return i;
+                }
+            }
+        }
+        return -1;
     }
 }
