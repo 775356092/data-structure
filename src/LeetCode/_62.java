7bc846cfb5b7c59ef1276a0d4bf48f1425e5b75b
wanghao <wh89801227@163.com>
2020-09-11 13:02:21 +0800

    不同路径

---
+++ src/LeetCode/_62.java
@@ -0,0 +1,28 @@
+package LeetCode;
+
+/**
+ * @description: 不同路径
+ * @author: wang hao
+ * @create: 2020-09-11 13:01
+ */
+public class _62 {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        //dp[m][n] = dp[m][n-1] + dp[m-1][n]
+        dp[0][0] = 1;
+        //上边界
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = dp[i - 1][0];
+        }
+        //左边界
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = dp[0][j - 1];
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
