d443937b3b546a02ed2e55a135f4e9bb17d98c9a
wanghao <wh89801227@163.com>
2020-09-14 08:52:38 +0800

    二叉树的中序遍历

---
+++ src/LeetCode/_94.java
@@ -0,0 +1,42 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @description: 二叉树的中序遍历
+ * @author: wang hao
+ * @create: 2020-09-14 08:51
+ */
+public class _94 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    ArrayList<Integer> res = new ArrayList<>();
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        dfs(root);
+        return res;
+    }
+
+    public void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        if (root.left == null && root.right == null) {
+            res.add(root.val);
+            return;
+        }
+        dfs(root.left);
+        res.add(root.val);
+        dfs(root.right);
+    }
+}
