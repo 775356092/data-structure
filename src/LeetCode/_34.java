4f70a47705f8f048ad854ece9bc933fb43ab62ac
wanghao <wh89801227@163.com>
2020-09-14 15:03:38 +0800

    在排序数组中查找元素的第一个和最后一个位置

--- src/LeetCode/_34.java
+++ src/LeetCode/_34.java
@@ -32,10 +32,10 @@ public class _34 {
             }
             return res;
         }
-        while (left < right) {
+        while (left <= right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] >= target) {
-                right = mid;
+                right = mid - 1;
             } else {
                 left = mid + 1;
             }
@@ -46,10 +46,10 @@ public class _34 {
         res[0] = left;
         left = 0;
         right = nums.length - 1;
-        while (left < right) {
+        while (left <= right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] > target) {
-                right = mid;
+                right = mid - 1;
             } else {
                 left = mid + 1;
             }
@@ -60,6 +60,6 @@ public class _34 {
 
     @Test
     public void test() {
-        System.out.println(Arrays.toString(searchRange(new int[]{2,2}, 2)));
+        System.out.println(Arrays.toString(searchRange(new int[]{5, 7, 7, 8, 8, 10}, 8)));
     }
 }
