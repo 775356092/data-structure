7bbc9ebbb8c905d7dc405aeddda32e7ad6d5be0d
wanghao <wh89801227@163.com>
2020-09-28 09:23:08 +0800

    填充每个节点的下一个右侧节点指针II

---
+++ src/LeetCode/_117.java
@@ -0,0 +1,53 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @description: 填充每个节点的下一个右侧节点指针II
+ * @author: wang hao
+ * @create: 2020-09-28 08:57
+ */
+public class _117 {
+    public Node connect(Node root) {
+        if (root == null) return null;
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            ArrayList<Node> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                Node node = queue.poll();
+                list.add(node);
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+            for (int i = 0; i < size - 1; i++) {
+                list.get(i).next = list.get(i + 1);
+            }
+        }
+        return root;
+    }
+
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+}
