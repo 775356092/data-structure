c4ef947d8970b53e4a671b3091bd5e55b3bec600
wanghao <wh89801227@163.com>
2020-09-29 10:24:13 +0800

    二叉树的前序遍历

--- src/LeetCode/_144.java
+++ src/LeetCode/_144.java
@@ -2,6 +2,7 @@ package LeetCode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * @description: 二叉树的前序遍历
@@ -34,6 +35,22 @@ public class _144 {
         res.add(root.val);
         dfs(root.left);
         dfs(root.right);
+    }
+
+    public List<Integer> preorderTraversal2(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode t = root;
+        while (t != null || !stack.isEmpty()) {
+            if (t != null) {
+                stack.push(t);
+                res.add(t.val);
+                t = t.left;
+            } else {
+                t = stack.pop().right;
+            }
+        }
+        return res;
     }
 
 }
