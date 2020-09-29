bbe2d153df72a25ff9ef82e9dc646394d5d50c1f
wanghao <wh89801227@163.com>
2020-09-29 13:52:18 +0800

    把数字翻译成字符串

--- src/LeetCode/O_46.java
+++ src/LeetCode/O_46.java
@@ -10,7 +10,7 @@ public class O_46 {
         char[] s = Integer.toString(num).toCharArray();
         int[] dp = new int[s.length + 1];
         dp[0] = dp[1] = 1;
-        // 0<=s[i - 1]s[i] <= 25, dp[i] = dp[i - 2] + dp[i - 1]
+        // 10 <= s[i - 1]s[i] <= 25, dp[i] = dp[i - 2] + dp[i - 1]
         for (int i = 1; i < s.length; i++) {
             int t = (s[i - 1] - '0') * 10 + (s[i] - '0');
             if (t >= 10 && t <= 25) dp[i + 1] = dp[i - 1] + dp[i];
