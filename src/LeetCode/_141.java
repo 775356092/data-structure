e31fef8a2482cb7dbaeb2ddfaceef2e970c9aab0
wanghao <wh89801227@163.com>
2020-10-09 10:13:42 +0800

    环形链表

--- src/LeetCode/_141.java
+++ src/LeetCode/_141.java
@@ -9,22 +9,44 @@ public class _141 {
     class ListNode {
         int val;
         ListNode next;
+
         ListNode(int x) {
             val = x;
             next = null;
         }
     }
 
+    //hash表
     public boolean hasCycle(ListNode head) {
         HashSet<ListNode> set = new HashSet<>();
-        while (head!=null){
-            if(set.contains(head)){
+        while (head != null) {
+            if (set.contains(head)) {
                 return true;
-            }else {
+            } else {
                 set.add(head);
             }
             head = head.next;
         }
+        return false;
+    }
+
+    //快慢指针
+    public boolean hasCycle2(ListNode head) {
+        if (head == null)
+            return false;
+        //快慢两个指针
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            //慢指针每次走一步
+            slow = slow.next;
+            //快指针每次走两步
+            fast = fast.next.next;
+            //如果相遇，说明有环，直接返回true
+            if (slow == fast)
+                return true;
+        }
+        //否则就是没环
         return false;
     }
 }
