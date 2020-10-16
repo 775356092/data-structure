c558a6a994ae06699e97e6fca088ff08e2269fbc
wanghao <wh89801227@163.com>
2020-10-16 09:26:54 +0800

    有序数组的平方

--- src/LeetCode/_977.java
+++ src/LeetCode/_977.java
@@ -1,8 +1,6 @@
 package LeetCode;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 
 /**
  * @program: data-structure
@@ -15,20 +13,12 @@ public class _977 {
      * 给定一个按非递减顺序排序的整数数组 A，返回每个数字的平方组成的新数组，要求也按非递减顺序排序。
      */
     public static int[] sortedSquares(int[] A) {
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 0; i < A.length; i++) {
-            list.add((A[i] * A[i]));
-        }
-        Collections.sort(list);
         int[] res = new int[A.length];
-        for (int i = 0; i < list.size(); i++) {
-            res[i] = list.get(i);
+        for (int i = 0; i < A.length; i++) {
+            res[i] = A[i] * A[i];
         }
+        Arrays.sort(res);
         return res;
     }
 
-    public static void main(String[] args) {
-        int[] ints = sortedSquares(new int[]{-7, -3, 2, 3, 11});
-        System.out.println(Arrays.toString(ints));
-    }
 }
