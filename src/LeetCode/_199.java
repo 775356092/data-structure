5cc245d1469eb94a947114238c15aa5d81efe0fb
wanghao <wh89801227@163.com>
2020-11-06 11:29:34 +0800

    二叉树的右视图

---
+++ src/LeetCode/_199.java
@@ -0,0 +1,40 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @description: 二叉树的右视图
+ * @author: wang hao
+ * @create: 2020-11-06 11:27
+ */
+public class _199 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<Integer> rightSideView(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (i == size - 1) res.add(node.val);
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+        }
+        return res;
+    }
+}
