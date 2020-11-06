1c9eeca769aeaf47817533e1314bd5918a705366
wanghao <wh89801227@163.com>
2020-11-06 11:20:33 +0800

    根据数字二进制下 1 的数目排序

--- src/LeetCode/_1356.java
+++ src/LeetCode/_1356.java
@@ -1,40 +1,69 @@
 package LeetCode;
 
-import java.util.Arrays;
+import org.junit.Test;
 
+import java.util.*;
+
+/**
+ * @description: 根据数字二进制下 1 的数目排序
+ * @author: wang hao
+ * @create: 2020-11-06 09:09
+ */
 public class _1356 {
-    //1356. 根据数字二进制下 1 的数目排序
-    //给你一个整数数组 arr 。请你将数组中的元素按照其二进制表示中数字 1 的数目升序排序。
-    //如果存在多个数字二进制中 1 的数目相同，则必须将它们按照数值大小升序排列。
-    //请你返回排序后的数组。
-    public static String[] sortByBits(int[] arr) {
-        String[] count = new String[]{"","","","","","","","","","","","","","","","","","","","","","","","","","","",""};
-        int[] res = new int[arr.length];
-        Arrays.sort(arr);
-        for(int i=0;i<arr.length;i++){
-            int t = 0;
-            for(int j=31;j>=0;j--){
-                if((arr[i]>>>j&1)==1){
-                    t++;
-                }
-            }
-            count[t]+="+"+(arr[i]);
+
+    public int[] sortByBits2(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = bitCount(arr[i]) * 10000000 + arr[i];
         }
-        int t = 0;
-/*        for(int i=0;i<32;i++){
-            if(t<arr.length){
-                for(int j=0;j<count[i].length();j++){
-                    res[t] = Integer.parseInt(count[i].substring(j,j+1));
-                    t++;
-                }
-            }else {
-                break;
-            }
-        }*/
-        return count;
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i] % 10000000;
+        }
+        return arr;
     }
 
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(sortByBits(new int[]{10,100,1000,1000,10000})));
+    public int[] sortByBits(int[] arr) {
+        // arr中每个数字二进制1的个数
+        int[] bit = new int[10001];
+        List<Integer> list = new ArrayList<>();
+        for (int x : arr) {
+            list.add(x);
+            bit[x] = bitCount(x);
+        }
+        list.sort((x, y) -> {
+            if (bit[x] != bit[y]) {
+                return bit[x] - bit[y];
+            } else {
+                return x - y;
+            }
+        });
+        for (int i = 0; i < arr.length; ++i) {
+            arr[i] = list.get(i);
+        }
+        return arr;
+    }
+
+    public int bitCount(int n) {
+        int res = 0;
+        while (n != 0) {
+            // 清除最低位的1
+            n &= (n - 1);
+            res++;
+        }
+        return res;
+    }
+
+    public int bitCount2(int n) {
+        int res = 0;
+        while (n != 0) {
+            if ((n & 1) == 1) res++;
+            n >>= 1;
+        }
+        return res;
+    }
+
+    @Test
+    public void test() {
+        System.out.println(Arrays.toString(sortByBits(new int[]{1024,512,256,128,64,32,16,8,4,2,1})));
     }
 }
